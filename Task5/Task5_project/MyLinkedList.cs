0725f599c9331aa536d0c662e822d35ad236865e
eflerrr <cool.yan_sv@mail.ru>
2023-07-10 20:52:33 +0300

    Task5 [LinkedList]: all done!

---
+++ Task5/Task5_project/MyLinkedList.cs
@@ -0,0 +1,303 @@
+﻿using System.Collections;
+
+
+namespace Task5_project;
+
+public sealed class MyLinkedList<T> : 
+    IEquatable<MyLinkedList<T>>,
+    ICloneable,
+    IEnumerable<T>
+{
+
+    private class MyLinkedListNode<T>
+    {
+        public T? Value { get; set; }
+        public MyLinkedListNode<T>? Prev { get; set; }
+        public MyLinkedListNode<T>? Next { get; set; }
+    }
+    
+    
+    private readonly LinkedList<T> _list;
+
+    
+    public MyLinkedList()
+    {
+        _list = new LinkedList<T>();
+    }
+
+    public MyLinkedList(MyLinkedList<T>? other)     //attribute?
+    {
+        if (ReferenceEquals(other, null)) throw new ArgumentNullException(nameof(other));
+        _list = new LinkedList<T>(other._list);
+    }
+
+    public MyLinkedList(IEnumerable<T>? other)      //attribute?
+    {
+        if (other == null) throw new ArgumentNullException(nameof(other));
+        _list = new LinkedList<T>(other);
+    }
+
+    public T this[int index]
+    {
+        get
+        {
+            if (index < 0) throw new ArgumentOutOfRangeException(nameof(index));
+            var node = _list.First;
+            if (node == null) throw new ArgumentOutOfRangeException(nameof(index));
+            for (var i = 0; i < index; ++i)
+            {
+                node = node.Next;
+                if (node == null) throw new ArgumentOutOfRangeException(nameof(index));
+            }
+
+            return node.Value;
+        }
+
+        set
+        {
+            Delete(index);
+            Insert(value, index);
+        }
+    }
+
+    public MyLinkedList<T> InsertFirst(T value)
+    {
+        _list.AddFirst(value);
+        return this;
+    }
+
+    public MyLinkedList<T> InsertLast(T value)
+    {
+        _list.AddLast(value);
+        return this;
+    }
+
+    public MyLinkedList<T> Insert(T value, int index)
+    {
+        if (index < 0) throw new ArgumentOutOfRangeException(nameof(index));
+        var node = _list.First;
+        if (node == null) throw new ArgumentOutOfRangeException(nameof(index));
+        for (var i = 0; i < index - 1 ; ++i)
+        {
+            node = node.Next;
+            if (node == null) throw new ArgumentOutOfRangeException(nameof(index));
+        }
+
+        _list.AddAfter(node, value);
+        
+        return this;
+    }
+
+    public MyLinkedList<T> DeleteFirst()
+    {
+        _list.RemoveFirst();
+        return this;
+
+    }
+
+    public MyLinkedList<T> DeleteLast()
+    {
+        _list.RemoveLast();
+        return this;
+    }
+
+    public MyLinkedList<T> Delete(int index)
+    {
+        if (index < 0) throw new ArgumentOutOfRangeException(nameof(index));
+        var node = _list.First;
+        if (node == null) throw new ArgumentOutOfRangeException(nameof(index));
+        for (var i = 0; i < index; ++i)
+        {
+            node = node.Next;
+            if (node == null) throw new ArgumentOutOfRangeException(nameof(index));
+        }
+
+        _list.Remove(node);
+        return this;
+    }
+
+    public MyLinkedList<T> Reverse()
+    {
+        return new MyLinkedList<T>(_list.Reverse());
+    }
+
+    public static MyLinkedList<T> operator !(MyLinkedList<T> source)
+    {
+        return source.Reverse();
+    }
+
+    public static MyLinkedList<T> Concat(MyLinkedList<T> x, MyLinkedList<T> y)
+    {
+        return new MyLinkedList<T>(x._list.Concat(y._list));
+    }
+    
+    public static MyLinkedList<T> operator +(MyLinkedList<T> x, MyLinkedList<T> y)
+    {
+        return Concat(x, y);
+    }
+
+    public static MyLinkedList<T> Intersection(MyLinkedList<T> x, MyLinkedList<T> y, IEqualityComparer<T> comparer)
+    {
+        return new MyLinkedList<T>(x._list.Intersect(y._list, comparer));
+    }
+
+    public static MyLinkedList<T> operator &(MyLinkedList<T> x, MyLinkedList<T> y)
+    {
+        return Intersection(x, y, EqualityComparer<T>.Default);     // default?
+    }
+
+    public static MyLinkedList<T> Unite(MyLinkedList<T> x, MyLinkedList<T> y, IEqualityComparer<T> comparer)
+    {
+        return new MyLinkedList<T>(x._list.Union(y._list, comparer));
+    }
+    
+    public static MyLinkedList<T> operator |(MyLinkedList<T> x, MyLinkedList<T> y)
+    {
+        return Unite(x, y, EqualityComparer<T>.Default);            // default?
+    }
+    
+    public static MyLinkedList<T> Subtraction(MyLinkedList<T> x, MyLinkedList<T> y, IEqualityComparer<T> comparer)
+    {
+        return new MyLinkedList<T>(x._list.Except(y._list, comparer));
+    }
+    
+    public static MyLinkedList<T> operator -(MyLinkedList<T> x, MyLinkedList<T> y)
+    {
+        return Subtraction(x, y, EqualityComparer<T>.Default);     // default?
+    }
+    
+    public MyLinkedList<T> Sorting(IComparer<T>? comparer)
+    {
+        if (comparer == null) throw new ArgumentNullException(nameof(comparer));
+        var comparerDelegate = new Comparison<T?>(comparer.Compare);
+        var result = new List<T>(_list);
+        
+        InsertionSort(result, comparerDelegate);
+
+        return new MyLinkedList<T>(result);
+    }
+    
+    public MyLinkedList<T> Sorting(Comparer<T>? comparer)
+    {
+        if (comparer == null) throw new ArgumentNullException(nameof(comparer));
+        var comparerDelegate = new Comparison<T?>(comparer.Compare);
+        var result = new List<T>(_list);
+        
+        InsertionSort(result, comparerDelegate);
+
+        return new MyLinkedList<T>(result);
+    }
+    
+    public MyLinkedList<T> Sorting(Comparison<T>? comparerDelegate)
+    {
+        if (comparerDelegate == null) throw new ArgumentNullException(nameof(comparerDelegate));
+        var result = new List<T>(_list);
+        
+        InsertionSort(result, comparerDelegate);
+
+        return new MyLinkedList<T>(result);
+    }
+
+    private void InsertionSort(List<T> arr, Comparison<T> comparer)
+    {
+        var len = arr.Count;
+        for (var i = 1; i < len; i++)
+        for (var j = i; j > 0 && comparer.Invoke(arr[j - 1], arr[j]) > 0; --j)
+        {
+            (arr[j - 1], arr[j]) = (arr[j], arr[j - 1]);
+        }
+    }
+
+    public void Action(Action<T>? action)
+    {
+        if (action == null) throw new ArgumentNullException(nameof(action));
+        var node = _list.First;
+        while (node != null)
+        {
+            action.Invoke(node.Value);
+            node = node.Next;
+        }
+    }
+
+    public static bool operator ==(MyLinkedList<T> x, MyLinkedList<T> y)
+    {
+        if (ReferenceEquals(x, null) && ReferenceEquals(y, null)) return true;
+        if (ReferenceEquals(x, null) && !ReferenceEquals(y, null)) return false;
+        if (!ReferenceEquals(x, null) && ReferenceEquals(y, null)) return false;
+        return x!.Equals(y);
+    }
+    
+    public static bool operator !=(MyLinkedList<T> x, MyLinkedList<T> y)
+    {
+        return !(x == y);
+    }
+
+    public static MyLinkedList<T> operator *(MyLinkedList<T>? x, MyLinkedList<T>? y)
+    {
+        if (ReferenceEquals(x, null)) throw new ArgumentNullException(nameof(x));
+        if (ReferenceEquals(y, null)) throw new ArgumentNullException(nameof(y));
+
+        var len = Math.Min(x._list.Count, y._list.Count);
+        var result = new MyLinkedList<T>();
+        var xNode = x._list.First;
+        var yNode = y._list.First;
+        
+        for (var i = 0; i < len; ++i)
+        {
+            var item = (xNode!.Value as dynamic) * (yNode!.Value as dynamic);
+            result._list.AddLast((T)item);
+            xNode = xNode.Next;
+            yNode = yNode.Next;
+        }
+
+        return result;
+    }
+
+    public override int GetHashCode()
+    {
+        return _list.GetHashCode();
+    }
+
+    public override string ToString()
+    {
+        return string.Join(' ', _list);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is MyLinkedList<T> list) return Equals(list);
+        return false;
+    }
+    
+    public bool Equals(MyLinkedList<T>? other)
+    {
+        if (ReferenceEquals(other, null)) return false;
+        if (!_list.Count.Equals(other._list.Count)) return false;
+        var xNode = _list.First;
+        var yNode = other._list.First;
+        while (xNode != null && yNode != null)
+        {
+            if (!xNode.Value!.Equals(yNode.Value)) return false;
+            xNode = xNode.Next;
+            yNode = yNode.Next;
+        }
+
+        return true;
+    }
+    
+    public object Clone()
+    {
+        return new MyLinkedList<T>(_list);
+    }
+    
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        return _list.GetEnumerator();
+    }
+    
+}
---
+++ Task5/Task5_project/Program.cs
@@ -0,0 +1,166 @@
+﻿namespace Task5_project;
+
+
+public static class Program
+{
+
+    private static int MyIntComparerFoo(int x, int y)
+    {
+        return x.CompareTo(y);
+    }
+
+    private static void MyActionFoo(int x)
+    {
+        Console.WriteLine($"action worked with {x}");
+    }
+    
+    public static void Main()
+    {
+        try
+        {
+
+            // constructor MyLinkedList<T>
+            // ToString
+            // InsertLast
+            // InsertFirst
+            
+            Console.WriteLine($"{Environment.NewLine}--- constructor MyLinkedList<T> ---{Environment.NewLine}" +
+                              $"--- ToString ---{Environment.NewLine}" +
+                              $"--- InsertLast ---{Environment.NewLine}" +
+                              $"--- InsertFirst ---{Environment.NewLine}");
+            var a = new MyLinkedList<int>();
+            Console.WriteLine(a);
+            a.InsertLast(1).InsertLast(2).InsertLast(35).InsertLast(4).InsertLast(5);
+            Console.WriteLine("a -> " + a);
+            var b = new MyLinkedList<int>(a);
+            Console.WriteLine("b -> " + b);
+            b.InsertFirst(0);
+            Console.WriteLine("b(after) -> " + b);
+
+            // constructor IEnumerable<T>
+            // indexer
+            // Insert
+            
+            Console.WriteLine($"{Environment.NewLine}--- constructor IEnumerable<T> ---{Environment.NewLine}" +
+                              $"--- indexer ---{Environment.NewLine}" +
+                              $"--- Insert ---{Environment.NewLine}");
+            Console.WriteLine(a[2]);
+            a[3] = 8;
+            Console.WriteLine("a(indexer) -> " + a);
+            var test = new[] { 4, 3, 2, 1 };
+            var c = new MyLinkedList<int>(test);
+            Console.WriteLine("c -> " + c);
+            c.Insert(800, 1);
+            Console.WriteLine("c(insert) -> " + c);
+            
+            // DeleteFirst
+            // DeleteLast
+            // Delete
+
+            Console.WriteLine($"{Environment.NewLine}--- DeleteFirst ---{Environment.NewLine}" +
+                              $"--- DeleteLast ---{Environment.NewLine}" +
+                              $"--- Delete ---{Environment.NewLine}");
+            c.DeleteFirst().DeleteLast().Delete(1);
+            Console.WriteLine("c(delete x3) -> " + c);
+            
+            // Reverse + operator!
+
+            Console.WriteLine($"{Environment.NewLine}--- Reverse + operator! ---{Environment.NewLine}");
+            a = a.Reverse();
+            Console.WriteLine("a(reverse) -> " + a);
+            Console.WriteLine("b(reverse) -> " + !b);
+            
+            // Concat + operator+
+            
+            Console.WriteLine($"{Environment.NewLine}--- Concat + operator+ ---{Environment.NewLine}");
+            var d = a + b;
+            var extra = MyLinkedList<int>.Concat(a, b);
+            Console.WriteLine("d(concat a + b) -> " + d);
+            
+            // Intersection + operator&
+            
+            Console.WriteLine($"{Environment.NewLine}--- Intersection + operator& ---{Environment.NewLine}");
+            var e = a & b;
+            Console.WriteLine("e(intersection a & b) -> " + e);
+            
+            // Unite + operator|
+            
+            Console.WriteLine($"{Environment.NewLine}--- Unite + operator| ---{Environment.NewLine}");
+            var f = a | b;
+            Console.WriteLine("f(unite a | b) -> " + f);
+            
+            // Subtraction + operator-
+            
+            Console.WriteLine($"{Environment.NewLine}--- Subtraction + operator- ---{Environment.NewLine}");
+            var g = a - b;
+            Console.WriteLine("g(subtract a - b) -> " + g);
+            
+            // Sorting
+            
+            Console.WriteLine($"{Environment.NewLine}--- Sorting ---{Environment.NewLine}");
+            var comp = new Comparison<int>(MyIntComparerFoo);
+            var h = f.Sorting(comp);
+            Console.WriteLine("h(sorting f) -> " + h);
+            
+            // Action
+            
+            Console.WriteLine($"{Environment.NewLine}--- Action ---{Environment.NewLine}");
+            var act = new Action<int>(MyActionFoo);
+            Console.WriteLine("h(action)");
+            h.Action(act);
+
+            // operator== operator!=
+            
+            Console.WriteLine($"{Environment.NewLine}--- operator== operator!= ---{Environment.NewLine}");
+            Console.WriteLine(a == b);
+            var i = new MyLinkedList<int>(h);
+            Console.WriteLine(i != h);
+
+            // operator*
+            
+            Console.WriteLine($"{Environment.NewLine}--- operator* ---{Environment.NewLine}");
+            Console.WriteLine("a -> " + a);
+            Console.WriteLine("b -> " + b);
+            Console.WriteLine($"a * b -> {a * b}");
+            
+            // GetHashCode
+            
+            Console.WriteLine($"{Environment.NewLine}--- GetHashCode ---{Environment.NewLine}");
+            Console.WriteLine("a(hash) -> " + a.GetHashCode());
+            
+            // ICloneable
+            
+            Console.WriteLine($"{Environment.NewLine}--- ICloneable ---{Environment.NewLine}");
+            Console.WriteLine("a -> " + a);
+            var clone = a.Clone() as MyLinkedList<int> ?? throw new Exception("impossible to throw");
+            clone.InsertLast(999);
+            Console.WriteLine("clone(+ insert 999) -> " + clone);
+            
+            // IEnumerable<T>
+            
+            Console.WriteLine($"{Environment.NewLine}--- IEnumerable<T> ---{Environment.NewLine}");
+            foreach (var item in clone)
+            {
+                Console.Write(item + " ~ ");
+            }
+
+        }
+        catch (ArgumentOutOfRangeException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        catch (ArgumentNullException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+    
+}
